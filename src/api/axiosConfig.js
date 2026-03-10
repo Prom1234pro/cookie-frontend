4344e932b299d96c24bc22392f8bb32b6f51174c
Prom1234pro <promiseimadonmwinyi@gmail.com>
2026-03-10 06:35:11 +0100

    cookies testing

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Configure Axios globally
 const apiClient = axios.create({
   // Use VITE_API_URL or fallback to Render URL
-  baseURL: import.meta.env.VITE_API_URL || 'https://mybackend.onrender.com', 
+  baseURL: import.meta.env.VITE_API_URL || 'https://cookie-backend-cfa3.onrender.com',
   withCredentials: true, // IMPORTANT: automatically sends cookies on cross-origin requests
   headers: {
     'Content-Type': 'application/json',
